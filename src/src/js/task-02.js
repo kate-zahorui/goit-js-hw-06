fa1839dedc8f48a3ceaf5387735f541323c59408
kate-zahorui <kate.zahorui@gmail.com>
2022-07-01 18:15:12 +0300

    Task 02 done

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -1,1 +1,12 @@
 const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+const ingredientItems = ingredients.map(ingredient => {
+	const item = document.createElement("li");
+	item.textContent = ingredient;
+	item.classList.add("item");
+	return item;
+});
+
+ingredientsRef.append(...ingredientItems);
