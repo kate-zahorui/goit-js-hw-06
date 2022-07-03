7577aae6f5d41aafd416c1a7aa9b7937b256d55b
kate-zahorui <kate.zahorui@gmail.com>
2022-07-03 12:35:10 +0300

    Task 08 done

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,19 @@
+const formRef = document.querySelector(".login-form");
 
+const onFormSubmit = function (event) {
+	event.preventDefault();
+	const { email, password } = formRef.elements;
+
+	const formValues = {
+		email: `${email.value}`,
+		password: `${password.value}`,
+	};
+	if (formValues.email === "" || formValues.password === "") {
+		alert("Всі поля повинні бути заповнені!");
+	} else {
+		console.log(formValues);
+		event.currentTarget.reset();
+	}
+};
+
+formRef.addEventListener("submit", onFormSubmit);
