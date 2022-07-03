3381ba88d75f298bcaf012a73d099c97a60ca0de
kate-zahorui <kate.zahorui@gmail.com>
2022-07-03 14:45:25 +0300

    Task 09 done

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+const bodyRef = document.body;
+const changeColorButton = document.querySelector("button.change-color");
+const colorName = document.querySelector("span.color");
+
+const onBgColorChanging = function () {
+	const newColor = getRandomHexColor();
+	bodyRef.style.backgroundColor = newColor;
+	colorName.textContent = newColor;
+};
+
+changeColorButton.addEventListener("click", onBgColorChanging);
